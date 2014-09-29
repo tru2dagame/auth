f5769b07a6109ac5e6175ef52327f61cdde03ff5
Sid <sid@ubnt.com>
2014-09-29 16:45:24 +0800

    add token session

--- s/weibo/callback.php
+++ s/weibo/callback.php
@@ -29,7 +29,7 @@ if (isset($_REQUEST['code'])) {
 }
 
 if ($token) {
-    $c = new SaeTClientV2( WEIBO_AKEY , WEIBO_SKEY , $_SESSION['token']['access_token'] );
+    $c = new SaeTClientV2( WEIBO_AKEY , WEIBO_SKEY , $token['access_token'] );
     $from_user_name = $c->get_uid();
     if (!$fromUserName) {
         header('Location: ' . DEFAULT_URL);
