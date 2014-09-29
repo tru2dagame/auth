fd4745d0c84672660bdac260f87d2eb47b9cf365
Sid <sid@ubnt.com>
2014-09-29 17:00:53 +0800

    test

--- s/weibo/callback.php
+++ s/weibo/callback.php
@@ -38,7 +38,7 @@ if ($token) {
         header('Location: ' . DEFAULT_URL);
         exit();
     }
-    $sql = "select * from " . WEIBO_TABLE . " where `Mac_ID` = '{$Mac_ID}' and `fromUserName` = '{$from_user_name}";
+    $sql = "select * from " . WEIBO_TABLE . " where `Mac_ID` = '{$Mac_ID}' and `fromUserName` = '{$from_user_name}'";
     $res = $mysql->query($sql, 'all');
 
     if (!is_array($res) || count($res) <= 0) {
